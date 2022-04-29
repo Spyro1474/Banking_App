ba20abaec0984e0882fb3f7b9831b883f56d112f
Spyro1474 <jgaab@umich.edu>
2022-04-29 15:28:54 -0400

    working

--- src/main/java/DAO/CheckingDAO.java
+++ src/main/java/DAO/CheckingDAO.java
@@ -1,5 +1,11 @@
 package DAO;
 
-public class CheckingDAO {
+public class CheckingDAO extends AccountDAO implements DAOInterface{
+
+	@Override
+	public void signup(String username, float balance, String accountType, int accountNum) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/main/java/DAO/SavingsDAO.java
+++ src/main/java/DAO/SavingsDAO.java
@@ -1,5 +1,11 @@
 package DAO;
 
-public class SavingsDAO {
+public class SavingsDAO extends AccountDAO implements DAOInterface {
+
+	@Override
+	public void signup(String username, float balance, String accountType, int accountNum) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
